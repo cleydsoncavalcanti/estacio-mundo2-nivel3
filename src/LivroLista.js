22ac6a9d243ac9d8a924e293b94811bdb0a7e784
Cavalcanti <cleydsoncavalcanti@gmail.com>
2023-07-29 20:52:52 -0300

    ✨ separando components

--- src/LivroLista.js
+++ src/LivroLista.js
@@ -1,30 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ControleLivro from './controle/ControleLivros';
-import ControleEditora from './controle/ControleEditora';
-
-const LinhaLivro = ({ livro, excluir }) => {
-  const controleEditora = new ControleEditora();
-  const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
-
-  return (
-    <tr className=''>
-      <td className='mx-2 my-3 px-3 d-flex flex-wrap'>
-        {livro.título}
-        <br />
-        <button className='btn btn-danger' onClick={() => excluir(livro.codigo)}>Excluir</button>
-      </td>
-      <td className='mx-2 my-3 px-3'>{livro.resumo}</td>
-      <td className='mx-2 my-3 px-3'>{nomeEditora}</td>
-      <td className='mx-2 my-3 px-3'>
-        <ul>
-          {livro.autores.map((autor, index) => (
-            <li key={index}>{autor}</li>
-          ))}
-        </ul>
-      </td>
-    </tr>
-  );
-};
+import LinhaLivro from './components/LinhaLivro';
 
 const LivroLista = () => {
   const [livros, setLivros] = useState([]);
---
+++ src/components/LinhaLivro.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ControleEditora from '../controle/ControleEditora';
+
+const LinhaLivro = ({ livro, excluir }) => {
+  const controleEditora = new ControleEditora();
+  const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
+
+  return (
+    <tr className=''>
+      <td className='mx-2 my-3 px-3 d-flex flex-wrap'>
+        {livro.título}
+        <br />
+        <button className='btn btn-danger' onClick={() => excluir(livro.codigo)}>Excluir</button>
+      </td>
+      <td className='mx-2 my-3 px-3'>{livro.resumo}</td>
+      <td className='mx-2 my-3 px-3'>{nomeEditora}</td>
+      <td className='mx-2 my-3 px-3'>
+        <ul>
+          {livro.autores.map((autor, index) => (
+            <li key={index}>{autor}</li>
+          ))}
+        </ul>
+      </td>
+    </tr>
+  );
+};
+
+export default LinhaLivro;
